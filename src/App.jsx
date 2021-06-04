5736d2be64a5fc8bb9e3154bb1370d0e6ebe0202
Andrey Sychev <nukemoore@gmail.com>
2021-06-04 19:33:44 +0300

    fix: add recoil root

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {RecoilRoot} from "recoil";
+
 import Header from "./Header";
 import "./index.css";
 
-const App = () => <div>This is header app <Header /></div>;
+const App = () => <RecoilRoot>This is header app <Header /></RecoilRoot>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
